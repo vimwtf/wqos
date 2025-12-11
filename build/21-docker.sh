7cddb7c9e174f786fd03b21c417c864b686b3c42
John Wq <wq@vim.wtf>
2025-12-11 16:55:26 -0600

    add iptable_net module for docker

--- build/21-docker.sh
+++ build/21-docker.sh
@@ -4,6 +4,18 @@
 set -oue pipefail
 
 echo "Installing Docker..."
+
+# Apply IP Forwarding before installing Docker to prevent messing with LXC networking
+sysctl -p
+
+# Load iptable_nat module for docker-in-docker.
+# See:
+#   - https://github.com/ublue-os/bluefin/issues/2365
+#   - https://github.com/devcontainers/features/issues/1235
+mkdir -p /etc/modules-load.d
+tee /etc/modules-load.d/ip_tables.conf <<EOF
+iptable_nat
+EOF
   
 dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/docker-ce.repo
