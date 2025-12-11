3a010010b329e24d79fd00cac52e9363168549bb
John Wq <wq@vim.wtf>
2025-12-11 00:00:09 -0600

    fix: SC2013

--- build/15-flatpaks.sh
+++ build/15-flatpaks.sh
@@ -5,9 +5,9 @@ set -oue pipefail
 
 echo "Copying Flatpak preinstalls..."
 
-for pak in $(cat /ctx/custom/flatpaks/wqos-flatpaks.list); do
-  type=${pak%/*}
-  id=${pak#*/}
+while IFS= read -r line; do
+  type=${line%/*}
+  id=${line#*/}
   if [ "$type" == "runtime" ]; then
     tee -a /etc/flatpak/preinstall.d/default.preinstall <<EOF
 [Flatpak Preinstall $id]
@@ -22,13 +22,13 @@ Branch=stable
 
 EOF
   fi
-done
+done < /ctx/custom/flatpaks/wqos-flatpaks.list
 
 echo "Copying custom Flatpak list..."
 cp /ctx/custom/flatpaks/wqos-flatpaks.list /etc/ublue-os/wqos-flatpaks.list
 
 echo "Removing unwanted Flatpaks from system list..."
-for pak in $(cat /ctx/custom/flatpaks/unwanted-flatpaks.list); do
-  grep -v "$pak" /etc/ublue-os/system-flatpaks.list > temp.list
+while IFS= read -r line; do
+  grep -v "$line" /etc/ublue-os/system-flatpaks.list > temp.list
   mv temp.list /etc/ublue-os/system-flatpaks.list
-done
+done < /ctx/custom/flatpaks/unwanted-flatpaks.list
